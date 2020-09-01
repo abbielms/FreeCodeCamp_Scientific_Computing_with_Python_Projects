d3971f95641dd365a261363ddf0f631f1f4f6d3b
Abbigail Elms <abbi_elms@hotmail.com>
2020-09-01 20:03:10 +0100

    Fixed bug in AM/PM switching

--- Time_Calculator/time_calculator.py
+++ Time_Calculator/time_calculator.py
@@ -1,4 +1,4 @@
-def add_time(start, duration):
+def add_time(start, duration, day='monday'):
     start_time, am_pm = start.split()
     start_hours, start_minutes = start_time.split(":")
     duration_hours, duration_minutes = duration.split(":")
@@ -14,14 +14,20 @@ def add_time(start, duration):
     final_minute = ((sum_minutes - 60) if sum_minutes >= 60 else sum_minutes)
     final_minute_format = (('0' + str(final_minute)) if len(str(final_minute)) == 1 else final_minute)
 
-    if (int(start_hours) + int(duration_hours) + (sum_minutes / 60)) > 12:
-        if am_pm == 'AM':
+    sum_hours_in_minutes = sum_hours * 60
+    sum_total_minutes = sum_hours_in_minutes + sum_minutes
+
+    def updated_period(am_or_pm):
+        if am_pm == 'PM' and (sum_total_minutes // 720) % 2 == 0:
+            period = 'PM'
+        elif am_pm == 'AM' and (sum_total_minutes // 720) % 2 == 1:
             period = 'PM'
         else:
             period = 'AM'
-    else:
-        period = am_pm
+        return period
 
-    new_time = f'{str(final_hour_format)}:{str(final_minute_format)} {period}'
+    updated_am_pm = updated_period(am_pm)
+
+    new_time = f'{str(final_hour_format)}:{str(final_minute_format)} {updated_am_pm}'
 
     return new_time
